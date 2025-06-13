a04e437e709efd0ad26663b12292ec51e821bfd1
rrr <rrr@yandex.by>
2025-06-13 15:32:05 +0300

    telephony demo has added

--- src/components/molecules/PortfolioProjectBlock/index.tsx
+++ src/components/molecules/PortfolioProjectBlock/index.tsx
@@ -50,41 +50,37 @@ const PortfolioProjectBlock: React.FC<IPortfolioProjectBlock> = ({
         ...animate,
         transition: { delay: animate.transition.delay + order / 10 },
       }}
-      className="z-20 bg-slate-800 rounded-lg px-3 sm:px-10 pb-5 pt-5 pointer-events-auto shadow-[1px_0px_15px_rgba(255,255,255,0.1) max-w-[750px] mx-auto"
+      className="z-20 bg-slate-800 rounded-lg px-3 sm:px-10 pb-5 pt-5 pointer-events-auto shadow-[1px_0px_15px_rgba(255,255,255,0.1) max-w-[900px] mx-auto"
     >
-      <div className="sm:hover:scale-[103%] transition-all duration-200">
-        <div>
-          <BlockTitle title={title} icon={icon} rightSideIcon={rightSideIcon} />
-        </div>
-        <p className="mb-4">{children}</p>
-        <Divider />
-        <div className="w-full flex justify-between mt-1">
-          <div className="flex items-center ">
-            <div className="border rounded-md p-2 gap-2 max-h-[60px] overflow-scroll grid grid-cols-4 sm:grid-cols-9 lg:grid-cols-12 xl:grid-cols-[20]">
-              {technologies?.map((item) => (
-                <a href={item.link} target="_blank" rel="noopener noreferrer">
-                  <img
-                    src={`${process.env.PUBLIC_URL}/${item.icon}`}
-                    alt={item.technology}
-                    width={30}
-                    height={30}
-                    className="object-contain w-[30px] h-[30px]"
-                  />
-                </a>
-              ))}
-            </div>
-          </div>
-          <div className="cursor-pointer flex justify-around items-center border rounded-md p-2 transition-all duration-200 hover:bg-slate-700 mx-2 w-[140px]">
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href={projectLink}
-              className="font-bold text-md"
-            >
-              Get familiar
-            </a>
+      <div>
+        <BlockTitle title={title} icon={icon} rightSideIcon={rightSideIcon} />
+      </div>
+      <p className="mb-4">{children}</p>
+      <Divider />
+      <div className="w-full flex justify-between mt-1">
+        <div className="flex items-center ">
+          <div className="border rounded-md p-2 gap-2 max-h-[60px] overflow-scroll grid grid-cols-4 sm:grid-cols-9 lg:grid-cols-12 xl:grid-cols-[20]">
+            {technologies?.map((item) => (
+              <a href={item.link} target="_blank" rel="noopener noreferrer">
+                <img
+                  src={`${process.env.PUBLIC_URL}/${item.icon}`}
+                  alt={item.technology}
+                  width={30}
+                  height={30}
+                  className="object-contain w-[30px] h-[30px]"
+                />
+              </a>
+            ))}
           </div>
         </div>
+        <a
+          className="cursor-pointer flex justify-around items-center font-bold text-md border rounded-md p-2 transition-all duration-200 hover:bg-slate-700 mx-2 w-[140px]"
+          target="_blank"
+          rel="noopener noreferrer"
+          href={projectLink}
+        >
+          Try out
+        </a>
       </div>
     </motion.div>
   );
