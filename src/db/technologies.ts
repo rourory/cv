469b8970ece306e37f1cff2d9694fa00670b019f
rrr <rrr@yandex.by>
2025-06-15 15:06:33 +0300

    before updating typescript

--- src/db/technologies.ts
+++ src/db/technologies.ts
@@ -1,205 +1,224 @@
 export const typescript: Technology = {
   technology: "TypeScript",
   icon: "icons/typescript.svg",
-  link: "https://www.typescriptlang.org/docs/"
+  link: "https://www.typescriptlang.org/docs/",
 };
 export const reactJs: Technology = {
   technology: "React.js",
   icon: "icons/react.svg",
-  link: "https://react.dev/"
+  link: "https://react.dev/",
 };
 export const reduxToolkit: Technology = {
   technology: "Redux Toolkit",
   icon: "icons/rtk.svg",
-  link: "https://redux-toolkit.js.org/"
+  link: "https://redux-toolkit.js.org/",
 };
 export const devExtremeUI: Technology = {
   technology: "DevExtremeUI",
   icon: "icons/devextreme.png",
-  link: "https://js.devexpress.com/Documentation/"
+  link: "https://js.devexpress.com/Documentation/",
 };
 export const npm: Technology = {
   technology: "NPM",
   icon: "icons/npm.svg",
-  link: "https://docs.npmjs.com/"
+  link: "https://docs.npmjs.com/",
 };
 export const materialUI: Technology = {
   technology: "MaterialUI",
   icon: "icons/materialui.svg",
-  link: "https://mui.com/"
+  link: "https://mui.com/",
 };
 export const electronJs: Technology = {
   technology: "Electron.js",
   icon: "icons/electron.svg",
-  link: "https://electronjs.org/docs/latest"
+  link: "https://electronjs.org/docs/latest",
 };
 export const nextJs: Technology = {
   technology: "Next.js",
   icon: "icons/nextjs.svg",
-  link: "https://nextjs.org/docs"
+  link: "https://nextjs.org/docs",
 };
-export const prisma : Technology = {
+export const prisma: Technology = {
   technology: "Prisma",
   icon: "icons/prisma.png",
-  link: "https://www.prisma.io/docs/"
-}
+  link: "https://www.prisma.io/docs/",
+};
 export const nodeJs: Technology = {
   technology: "Node.js",
   icon: "icons/nodejs.svg",
-  link: "https://nodejs.org/en/docs/"
+  link: "https://nodejs.org/en/docs/",
 };
 export const threeJs: Technology = {
   technology: "Three.js / RTF",
   icon: "icons/threejs.png",
-  link: "https://threejs.org/docs/"
+  link: "https://threejs.org/docs/",
 };
 export const shadcn: Technology = {
   technology: "Shadcn",
   icon: "icons/shadcn.png",
-  link: "https://ui.shadcn.com/docs"
+  link: "https://ui.shadcn.com/docs",
 };
 export const zustand: Technology = {
   technology: "Zustand",
   icon: "icons/zustand.svg",
-  link: "https://zustand-demo.pmnd.rs/"
+  link: "https://zustand-demo.pmnd.rs/",
 };
 export const jdbc: Technology = {
   technology: "JDBC",
   icon: "icons/jdbc.svg",
-  link: "https://docs.oracle.com/javase/8/docs/technotes/guides/jdbc/"
+  link: "https://docs.oracle.com/javase/8/docs/technotes/guides/jdbc/",
 };
 export const springBoot3: Technology = {
   technology: "Spring Boot 3",
   icon: "icons/springboot.svg",
-  link: "https://docs.spring.io/spring-boot/docs/current/reference/html/"
+  link: "https://docs.spring.io/spring-boot/docs/current/reference/html/",
 };
 export const springDataJPA: Technology = {
   technology: "Spring Data JPA",
   icon: "icons/springdata.png",
-  link: "https://docs.spring.io/spring-data/jpa/docs/current/reference/html/"
+  link: "https://docs.spring.io/spring-data/jpa/docs/current/reference/html/",
 };
 export const springWeb: Technology = {
   technology: "Spring Web",
   icon: "icons/springweb.svg",
-  link: "https://docs.spring.io/spring-framework/docs/current/reference/html/web.html"
+  link: "https://docs.spring.io/spring-framework/docs/current/reference/html/web.html",
 };
 export const springFramework: Technology = {
   technology: "Spring Framework",
   icon: "icons/springframework.svg",
-  link: "https://docs.spring.io/spring-framework/docs/current/reference/html/"
+  link: "https://docs.spring.io/spring-framework/docs/current/reference/html/",
 };
 export const hibernate5: Technology = {
   technology: "Hibernate 5",
   icon: "icons/hibernate.webp",
-  link: "https://docs.jboss.org/hibernate/orm/5.6/userguide/html_single/Hibernate_User_Guide.html"
+  link: "https://docs.jboss.org/hibernate/orm/5.6/userguide/html_single/Hibernate_User_Guide.html",
 };
 export const queryDSL: Technology = {
   technology: "QueryDSL",
   icon: "icons/querydsl.png",
-  link: "http://www.querydsl.com/static/querydsl/latest/reference/html/"
+  link: "http://www.querydsl.com/static/querydsl/latest/reference/html/",
 };
 export const gradle: Technology = {
   technology: "Gradle",
   icon: "icons/gradle.svg",
-  link: "https://docs.gradle.org/current/userguide/userguide.html"
+  link: "https://docs.gradle.org/current/userguide/userguide.html",
 };
 export const liquibase: Technology = {
   technology: "Liquibase",
   icon: "icons/liquibase.svg",
-  link: "https://docs.liquibase.com/"
+  link: "https://docs.liquibase.com/",
 };
 export const voskAPI: Technology = {
   technology: "VOSK API",
   icon: "icons/vosk.png",
-  link: "https://alphacephei.com/vosk/"
+  link: "https://alphacephei.com/vosk/",
 };
 export const swingUI: Technology = {
   technology: "Swing UI",
   icon: "icons/java.svg",
-  link: "https://docs.oracle.com/javase/8/docs/technotes/guides/swing/"
+  link: "https://docs.oracle.com/javase/8/docs/technotes/guides/swing/",
 };
 export const swaggerOpenAPI: Technology = {
   technology: "Swagger OpenAPI",
   icon: "icons/swagger.png",
-  link: "https://swagger.io/docs/specification/about/"
+  link: "https://swagger.io/docs/specification/about/",
 };
 export const lombok: Technology = {
   technology: "Lombok",
   icon: "icons/lombok.jpg",
-  link: "https://projectlombok.org/features/all"
+  link: "https://projectlombok.org/features/all",
 };
 export const springSecurity: Technology = {
   technology: "Spring Security",
   icon: "icons/springsecurity.svg",
-  link: "https://docs.spring.io/spring-security/reference/"
+  link: "https://docs.spring.io/spring-security/reference/",
 };
 export const apacheKafka: Technology = {
   technology: "Apache Kafka",
   icon: "icons/kafka.svg",
-  link: "https://kafka.apache.org/documentation/"
+  link: "https://kafka.apache.org/documentation/",
 };
 export const vscode: Technology = {
   technology: "Visual Studio Code",
   icon: "icons/vscode.svg",
-  link: "https://code.visualstudio.com/docs"
+  link: "https://code.visualstudio.com/docs",
 };
 export const intellijidea: Technology = {
   technology: "Intellij IDEA",
   icon: "icons/intellijidea.svg",
-  link: "https://www.jetbrains.com/idea/documentation/"
+  link: "https://www.jetbrains.com/idea/documentation/",
 };
 export const sketchup: Technology = {
   technology: "SketchUp 2022",
   icon: "icons/sketchup.png",
-  link: "https://help.sketchup.com/en/sketchup/2022"
+  link: "https://help.sketchup.com/en/sketchup/2022",
 };
 export const postgresql: Technology = {
   technology: "PostgreSQL",
   icon: "icons/postgresql.png",
-  link: "https://www.postgresql.org/docs/current/"
+  link: "https://www.postgresql.org/docs/current/",
 };
 export const vs2022: Technology = {
   technology: "Visual Studio 2022",
   icon: "icons/vs2022.png",
-  link: "https://learn.microsoft.com/visualstudio/"
+  link: "https://learn.microsoft.com/visualstudio/",
 };
 export const git: Technology = {
   technology: "Git",
   icon: "icons/git.svg",
-  link: "https://git-scm.com/doc"
+  link: "https://git-scm.com/doc",
 };
 export const postman: Technology = {
   technology: "Postman",
   icon: "icons/postman.svg",
-  link: "https://learning.postman.com/docs/"
+  link: "https://learning.postman.com/docs/",
 };
 export const blender: Technology = {
   technology: "Blender",
   icon: "icons/blender.png",
-  link: "https://docs.blender.org/manual/en/latest/"
+  link: "https://docs.blender.org/manual/en/latest/",
 };
 export const docker: Technology = {
   technology: "Docker",
   icon: "icons/docker.svg",
-  link: "https://docs.docker.com/"
+  link: "https://docs.docker.com/",
 };
 export const mlWorkflow: Technology = {
   technology: "ML Workflow",
   icon: "icons/ml.png",
-  link: "https://mlflow.org/docs/latest/index.html"
+  link: "https://mlflow.org/docs/latest/index.html",
 };
+
+export const flaUI: Technology = {
+  technology: "FlaUI",
+  icon: "icons/flaui.png",
+  link: "https://github.com/FlaUI",
+};
+
+export const fleck: Technology = {
+  technology: "Fleck",
+  icon: "icons/fleck.png",
+  link: "https://github.com/statianzo/Fleck",
+};
+
+export const nAudio: Technology = {
+  technology: "NAudio",
+  icon: "icons/naudio.png",
+  link: "https://www.nuget.org/packages/naudio",
+};
+
 export const russian: Technology = {
   technology: "Russian",
   icon: "icons/russian.webp",
-  link: ""  // нет документации
+  link: "", // нет документации
 };
 export const english: Technology = {
   technology: "English",
   icon: "icons/english.webp",
-  link: ""  // нет документации
+  link: "", // нет документации
 };
 export const german: Technology = {
   technology: "German",
   icon: "icons/german.webp",
-  link: ""  // нет документации
+  link: "", // нет документации
 };
